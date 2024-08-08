7062d24fe7851446d9eebd759782f3b0c552d8ed
namdtavailable <163511609+namdtavailable@users.noreply.github.com>
2024-08-08 15:48:29 +0700

    add command kill old process celery

--- start_celery.sh
+++ start_celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source zulip-py3-venv/bin/activate
-sudo ps auxww | grep 'celery worker' | awk '{print $2}' | xargs kill -9
+sudo ps auxww | grep 'celery' | awk '{print $2}' | xargs kill -9
 celery -A zproject worker -l info
