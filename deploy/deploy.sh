af0e745efc1e5840426f9feba4afd8ddc9aac5d7
hao.nguyendang <hao.nguyendang@vietis.com.vn>
2024-08-13 12:11:29 +0700

    allow execute to deploy file

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,5 @@ set -e
 sudo su zulip -c 'git -C /home/zulip/deployments/current pull'
 sudo su -c '/home/zulip/deployments/current/zulip-current-venv/bin/pip install --use-deprecated=legacy-resolver --no-deps --require-hashes -r /home/zulip/deployments/current/requirements/prod.txt'
 sudo su zulip -c '/home/zulip/deployments/current/manage.py migrate'
-sudo su zulip -c '/home/zulip/deployments/current/manage.py collectstatic --clear' # clear folder static before gathering from all static_dir
 sudo su zulip -c '/home/zulip/deployments/current/tools/update-prod-static' # in this command, do collect static again, do generate new static and copy all to static_root
 sudo su zulip -c '/home/zulip/deployments/current/scripts/restart-server'
