d7466d6da318b77d94b8b8eadef6645bd8da4fe2
hao.nguyendang <hao.nguyendang@vietis.com.vn>
2024-10-26 13:49:57 +0700

    add cicd

---
+++ deploy/install.sh
@@ -0,0 +1,1 @@
+/home/zulip/deployments/current/zulip-current-venv/bin/pip install --use-deprecated=legacy-resolver --no-deps --require-hashes -r /home/zulip/deployments/current/requirements/prod.txt
