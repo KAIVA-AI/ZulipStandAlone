ff6a54eaa391d78c93bb7e5f5a51752193dab568
hao.nguyendang <hao.nguyendang@vietis.com.vn>
2024-12-05 04:10:08 +0700

    fix sync realm metadata error when value is None

--- zerver/actions/realm_metadata.py
+++ zerver/actions/realm_metadata.py
@@ -9,12 +9,18 @@ from zerver.models import (
 
 @transaction.atomic
 def delete_realm_metadata_by_key(realm: Realm, key: str) -> None:
-    RealmMetadata.objects.filter(realm=realm, key=key).delete()
+    try:
+        RealmMetadata.objects.filter(realm=realm, key=key).delete()
+    except:
+        pass
 
 
 @transaction.atomic
 def delete_realm_metadata(realm: Realm) -> None:
-    RealmMetadata.objects.filter(realm=realm).delete()
+    try:
+        RealmMetadata.objects.filter(realm=realm).delete()
+    except:
+        pass
 
 
 @transaction.atomic
@@ -23,11 +29,14 @@ def set_realm_metadata(realm: Realm, metadata_list: list[dict]) -> None:
     for metadata in metadata_list:
         key = metadata["key"]
         value = metadata["value"]
-        RealmMetadata.objects.update_or_create(
-            realm=realm,
-            key=key,
-            defaults={
-                "value": value,
-                "created_at": current_time,
-            },
-        )
+        try:
+            RealmMetadata.objects.update_or_create(
+                realm=realm,
+                key=key,
+                defaults={
+                    "value": value or "",
+                    "created_at": current_time,
+                },
+            )
+        except:
+            pass
