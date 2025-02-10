c0a4d4aca4daf9914bae99cd9850bbf093b46423
namdtavailable <163511609+namdtavailable@users.noreply.github.com>
2025-02-10 15:57:40 +0700

    add allow cors, readme install zulip

--- zproject/computed_settings.py
+++ zproject/computed_settings.py
@@ -253,10 +253,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
-CORS_ALLOWED_ORIGINS = [
-    "https://kdev.kaiva.io/"
-]
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = ["*"]
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = (
     "DELETE",
