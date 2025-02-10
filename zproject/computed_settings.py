10215fbcb752f57ab89c24fe2a81811f24185e42
namdtavailable <163511609+namdtavailable@users.noreply.github.com>
2025-02-10 15:46:18 +0700

    add allow cors, readme install zulip

--- zproject/computed_settings.py
+++ zproject/computed_settings.py
@@ -253,7 +253,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
 ]
-
+CORS_ALLOWED_ORIGINS = []
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOW_METHODS = (
